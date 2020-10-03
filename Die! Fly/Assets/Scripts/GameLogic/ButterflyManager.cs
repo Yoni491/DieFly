935d9264b82bd2b12ef72904b0996528253cf940
Yoni491 <yoniendyoni@gmail.com>
2020-10-03 16:28:49 +0300

    fixed flys flying
    and more

--- Die! Fly/Assets/Scripts/GameLogic/ButterflyManager.cs
+++ Die! Fly/Assets/Scripts/GameLogic/ButterflyManager.cs
@@ -2,51 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum eSpawnPoints
+{
+    START,
+    COOKIE
+};
+
 public class ButterflyManager : MonoBehaviour
 {
-     [SerializeField]
-     private Transform[] m_SpawnPoints = null;
-     [SerializeField]
-     private Transform[] m_PlayerInteractionPoints = null; 
-     // This points mark the square around the player which the butterflys start flying around the player.
+    [SerializeField]
+    private Transform[] m_SpawnPoints = null;
+    [SerializeField]
+    private Transform[] m_PlayerInteractionPoints = null;// right - 0, left - 1
+    [SerializeField]
+    private Transform[] m_FoodPoints = null;
+    [SerializeField]
+    private GameObject[] m_ButterflyPrefabs = null;
+    // This points mark the square around the player which the butterflys start flying around the player.
 
-     private float m_Timer;
-     private float m_TimeToCreateButterfly;
-     void Start()
-     {
-          m_TimeToCreateButterfly = Random.Range(2, 3);
-     }
+    public static Transform[] s_PlayerInteractionPoints;
+    public static Transform[] s_FoodPoints;
+    private float m_Timer;
+    private float m_TimeToCreateButterfly;
 
-     void Update()
-     {
-          m_Timer += Time.deltaTime;
-          if (m_Timer >= m_TimeToCreateButterfly)
-          {
-               m_Timer = 0;
-               m_TimeToCreateButterfly = Random.Range(2, 3);
-               addNewButterflyToGame();
-          }
-     }
+    void Start()
+    {
+       s_PlayerInteractionPoints = m_PlayerInteractionPoints;
+       s_FoodPoints = m_FoodPoints;
+       m_TimeToCreateButterfly = Random.Range(0, 1);
+    }
 
-     private void addNewButterflyToGame()
-     {
+    void Update()
+    {
+         m_Timer += Time.deltaTime;
+         if (m_Timer >= m_TimeToCreateButterfly)
+         {
+              m_Timer = 0;
+              m_TimeToCreateButterfly = Random.Range(4, 5);
+              addNewButterflyToGame();
+         }
+    }
 
-     }
+    private void addNewButterflyToGame()
+    {
+
+        int butterflyNum = Random.Range(0, 2);
+        //Butterfly newButterfly;
+        GameObject butterflyPrefab=null;
+        int life=0;
+        float speed = 1.3f;
+        if (butterflyNum == 0)
+        {
+            life = 1;
+        }
+        else if (butterflyNum == 1)
+        {
+            life = 3;
+        }
+        butterflyPrefab = m_ButterflyPrefabs[butterflyNum];
+        GameObject g_butterfly = Instantiate(butterflyPrefab, m_SpawnPoints[0].position, Quaternion.identity, this.transform);
+        g_butterfly.GetComponent<ButterflyScript>().CreateButterfly(life, speed, 1);
+    }
 }
 
-public class Butterfly
-{
-     private int m_CurrentLife, m_MaxLife;
-     private float m_Speed;
-     private int m_FlyingMethod;
-     //private AudioClip m_Sound;
-     // material, prefabs
-
-     public Butterfly(int i_CurrentLife, int i_MaxLife, float i_Speed, int i_FlyingMethod)
-     {
-          m_CurrentLife = i_CurrentLife;
-          m_MaxLife = i_MaxLife;
-          m_Speed = i_Speed;
-          m_FlyingMethod = i_FlyingMethod;
-     }
-}
